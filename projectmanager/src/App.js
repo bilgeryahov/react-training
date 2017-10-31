cb4ab6ded5a6520ddad1281a98ca843b91fe2596
Bilger Yahov <bilger@studyportals.com>
2017-10-31 16:13:34 +0100

    Set the state in a lifecycle method.

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -6,21 +6,32 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            projects:[
-                {
-                    title: 'Business Website',
-                    category: 'Web Design'
-                },
-	            {
-		            title: 'Social App',
-		            category: 'Mobile Development'
-	            },
-	            {
-		            title: 'E-commerce Shopping Card',
-		            category: 'Web Development'
-	            }
-            ]
+            projects:[]
         }
+    }
+
+	/**
+	 * Lifecycle method.
+	 * Will trigger every time the component is re-rendered.
+	 */
+
+	componentWillMount(){
+		this.setState({
+			projects: [
+				{
+					title: 'Business Website',
+					category: 'Web Design'
+				},
+				{
+					title: 'Social App',
+					category: 'Mobile Development'
+				},
+				{
+					title: 'E-commerce Shopping Card',
+					category: 'Web Development'
+				}
+			]
+		});
     }
 
   render() {
